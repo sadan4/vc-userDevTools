af01997d2fad27105968f1c9e02a382f60334c63
sadan <117494111+sadan4@users.noreply.github.com>
2025-01-08 14:02:00 -0500

    create plugin UserDevTools

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "UserDevTools",
+    description: "",
+    authors: [
+        {
+            name: "sadan",
+            id: 521819891141967883n
+        }
+    ]
+})
